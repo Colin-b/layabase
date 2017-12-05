eaa3813e45576f4c36f0996d940bd8a60cf5c5f1
WW5328 <jeremy.langlois@external.engie.com>
2017-12-05 14:43:14 +0100

    Add testing on handle_exception method.

--- test/tests.py
+++ test/tests.py
@@ -2395,5 +2395,101 @@ class SQlAlchemyColumnsTest(unittest.TestCase):
         self.assertEqual('Python field type cannot be guessed for None field.', cm.exception.args[0])
 
 
+class FlaskRestPlusErrorsTest(unittest.TestCase):
+    def setUp(self):
+        logger.info(f'-------------------------------')
+        logger.info(f'Start of {self._testMethodName}')
+
+    def tearDown(self):
+        logger.info(f'End of {self._testMethodName}')
+        logger.info(f'-------------------------------')
+
+    def test_handle_exception_failed_validation_on_list_of_items(self):
+        class TestAPI:
+
+            @staticmethod
+            def model(cls, name):
+                pass
+
+            @staticmethod
+            def errorhandler(cls):
+                pass
+
+            @staticmethod
+            def marshal_with(cls, code):
+                def wrapper(func):
+                    # Mock of the input (List of items)
+                    received_data = [{'optional_string_value': 'my_value1', 'mandatory_integer_value': 1,
+                                      'optional_enum_value': 'First Enum Value', 'optional_date_value': '2017-10-23',
+                                      'optional_date_time_value': '2017-10-24T21:46:57.12458+00:00',
+                                      'optional_float_value': 100},
+                                     {'optional_string_value': 'my_value2', 'optional_enum_value': 'First Enum Value',
+                                      'optional_date_value': '2017-10-23',
+                                      'optional_date_time_value': '2017-10-24T21:46:57.12458+00:00',
+                                      'optional_float_value': 200}]
+                    failed_validation = flask_restplus_errors.ValidationFailed(received_data)
+                    errors = {1: {'mandatory_integer_value': ['Missing data for required field.']}}
+                    setattr(failed_validation, 'errors', errors)
+                    # Call handle_exception method
+                    result = func(failed_validation)
+                    # Assert output, if NOK will raise Assertion Error
+                    assert (result[0] == {'fields': [{'field_name': 'mandatory_integer_value on item number: 1.',
+                                                      'messages': ['Missing data for required field.']}]})
+                    assert (result[1] == 400)
+                    return result
+
+                return wrapper
+
+        # Since TestApi is not completely mocked, add_failed_validation_handler will raise a "NoneType is not iterable
+        # exception after the call to handle_exception. Exception is therefore ignored
+        try:
+            flask_restplus_errors.add_failed_validation_handler(TestAPI)
+        except TypeError:
+            pass
+
+    def test_handle_exception_failed_validation_a_single_item(self):
+        class TestAPI:
+
+            @staticmethod
+            def model(cls, name):
+                pass
+
+            @staticmethod
+            def errorhandler(cls):
+                pass
+
+            @staticmethod
+            def marshal_with(cls, code):
+                def wrapper(func):
+                    # Mock of the input (Single item)
+                    received_data = {'optional_string_value': 'my_value1', 'mandatory_integer_value': 1,
+                                     'optional_enum_value': 'First Enum Value', 'optional_date_value': '2017-10-23',
+                                     'optional_date_time_value': '2017-10-24T21:46:57.12458+00:00',
+                                     'optional_float_value': 100}, {'optional_string_value': 'my_value2',
+                                                                    'optional_enum_value': 'First Enum Value',
+                                                                    'optional_date_value': '2017-10-23',
+                                                                    'optional_date_time_value': '2017-10-24T21:46:57.12458+00:00',
+                                                                    'optional_float_value': 200}
+                    failed_validation = flask_restplus_errors.ValidationFailed(received_data)
+                    errors = {'mandatory_integer_value': ['Missing data for required field.']}
+                    setattr(failed_validation, 'errors', errors)
+                    # Call handle_exception method
+                    result = func(failed_validation)
+                    # Assert output, if NOK will raise Assertion Error
+                    assert (result[0] == {'fields': [
+                        {'field_name': 'mandatory_integer_value', 'messages': ['Missing data for required field.']}]})
+                    assert (result[1] == 400)
+                    return result
+
+                return wrapper
+
+        # Since TestApi is not completely mocked, add_failed_validation_handler will raise a "NoneType is not iterable
+        # exception after the call to handle_exception. Exception is therefore ignored
+        try:
+            flask_restplus_errors.add_failed_validation_handler(TestAPI)
+        except TypeError:
+            pass
+
+
 if __name__ == '__main__':
     unittest.main()
