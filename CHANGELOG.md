7b2f2f29d180e0d7548bb10958fad7ef81177a16
JS5391 <colin.bounouar@engie.com>
2019-01-11 13:05:51 +0100

    Update dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,12 @@ List all changes in various categories:
 * Bug fixes
 * Known issues
 
+## Version 13.15.0 (2019-01-11) ##
+
+### Enhancements ###
+
+- Update dependencies to latest version (pycommon_test 4.10.1, pycommon_error 2.16.0).
+
 ## Version 13.14.0 (2019-01-10) ##
 
 ### Enhancements ###
--- setup.py
+++ setup.py
@@ -15,12 +15,12 @@ setup(
     ]),
     install_requires=[
         # Used to handle errors
-        'pycommon-error==2.15.0'
+        'pycommon-error==2.16.0'
     ],
     extras_require={
         'testing': [
             # Used to provide testing help
-            'pycommon-test==4.10.0',
+            'pycommon-test==4.10.1',
             # Used to Manage Mongo Database
             'mongomock==3.14.0',
         ],
