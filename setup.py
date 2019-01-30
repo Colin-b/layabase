65a2dae56aa67d7be9ddf4cc452ac9f3ecfaeaa9
WW5328 <jeremy.langlois@external.engie.com>
2019-01-30 11:29:38 +0100

    Fix changelog and version Add all requirements to testing deps

--- setup.py
+++ setup.py
@@ -1,9 +1,36 @@
 import os
+
 from setuptools import setup, find_packages
 
 this_dir = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_dir, "README.md"), "r") as f:
     long_description = f.read()
+
+extra_requirements = {
+    "testing": [
+        # Used to provide testing help
+        "pycommon-test==5.0.0"
+    ],
+    "mongo": [
+        "pymongo[tls]==3.7.2",
+        # Used to manage date and datetime deserialization
+        "iso8601==0.1.12",
+    ],
+    # Used to Manage Non-Mongo Database
+    "sqlalchemy": [
+        "marshmallow==2.18.0",
+        "SQLAlchemy==1.2.16",
+        "marshmallow_sqlalchemy==0.15.0",
+    ],
+}
+
+# Add all extra requirements to testing
+extra_requirements["testing"] += [
+    extra
+    for extra_name, extra_list in extra_requirements.items()
+    if extra_name != "testing"
+    for extra in extra_list
+]
 
 setup(
     name="pycommon_database",
@@ -18,24 +45,5 @@ setup(
         # Used to handle errors
         "pycommon-error==2.15.0"
     ],
-    extras_require={
-        "testing": [
-            # Used to provide testing help
-            "pycommon-test==5.0.1",
-            # Used to Manage Mongo Database
-            "mongomock==3.14.0",
-        ],
-        # Used to Manage Mongo Database
-        "mongo": [
-            "pymongo[tls]==3.7.2",
-            # Used to manage date and datetime deserialization
-            "iso8601==0.1.12",
-        ],
-        # Used to Manage Non-Mongo Database
-        "sqlalchemy": [
-            "marshmallow==2.18.0",
-            "SQLAlchemy==1.2.16",
-            "marshmallow_sqlalchemy==0.15.0",
-        ],
-    },
+    extras_require=extra_requirements,
 )
