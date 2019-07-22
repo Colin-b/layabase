2e5cac0004c39f87576939e92e1977de9d99740f
JS5391 <colin.bounouar@engie.com>
2019-07-22 15:39:53 +0200

    Get rid of pycommon_test

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,13 +6,14 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
-## [13.22.0] - 2019-07-19
+## [13.22.0] - 2019-07-22
 ### Changed
 - Update mongomock to version 3.17.0
 - Update marshmallow to version 2.19.5
-- Update SQLAlchemy to version 1.3.5
+- Update SQLAlchemy to version 1.3.6
 - Update marshmallow_sqlalchemy to version 0.17.0
 - Update pycommon_error to version 2.20.0
+- Drop pycommon_test for pytest
 
 ## [13.21.0] - 2019-05-08
 ### Changed
--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import os
 
 from setuptools import setup, find_packages
-from pycommon_database.version import __version__
 
 this_dir = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_dir, "README.md"), "r") as f:
@@ -9,8 +8,8 @@ with open(os.path.join(this_dir, "README.md"), "r") as f:
 
 extra_requirements = {
     "testing": [
-        # Used to provide testing help
-        "pycommon_test==11.0.0",
+        # Used to run test cases
+        "pytest==5.0.1",
         # Used to manage testing of Mongo
         "mongomock==3.17.0",
     ],
@@ -22,7 +21,7 @@ extra_requirements = {
     # Used to Manage Non-Mongo Database
     "sqlalchemy": [
         "marshmallow==2.19.5",
-        "SQLAlchemy==1.3.5",
+        "SQLAlchemy==1.3.6",
         "marshmallow_sqlalchemy==0.17.0",
     ],
 }
@@ -37,9 +36,13 @@ extra_requirements["testing"] += [
 
 setup(
     name="pycommon_database",
-    version=__version__,
+    version=open("pycommon_database/version.py")
+    .readlines()[-1]
+    .split()[-1]
+    .strip("\"'"),
     description="Common Database handling",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(exclude=["test"]),
     install_requires=[
         # Used to handle errors
@@ -48,7 +51,21 @@ setup(
     extras_require=extra_requirements,
     python_requires=">=3.6",
     project_urls={
-        "Changelog": "https://github.tools.digital.engie.com/GEM-Py/pycommon-database/blob/development/CHANGELOG.md",
-        "Issues": "https://github.tools.digital.engie.com/GEM-Py/pycommon-database/issues",
+        "Changelog": "https://github.tools.digital.engie.com/GEM-Py/pycommon_database/blob/development/CHANGELOG.md",
+        "Issues": "https://github.tools.digital.engie.com/GEM-Py/pycommon_database/issues",
     },
+    license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Build Tools",
+    ],
+    keywords=["mongo", "sqla", "db", "flask"],
+    platforms=["Windows", "Linux"],
 )
