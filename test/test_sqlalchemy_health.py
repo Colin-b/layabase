ce6d291006ba7b7dd293a7ebe6bcda0bb0781079
JS5391 <colin.bounouar@engie.com>
2019-10-01 12:15:52 +0200

    Split health related test cases from model related test cases

---
+++ test/test_sqlalchemy_health.py
@@ -0,0 +1,45 @@
+import pytest
+import sqlalchemy
+
+
+import layabase
+import layabase.database_sqlalchemy
+import layabase.testing
+from test import DateTimeModuleMock
+
+
+@pytest.fixture
+def db():
+    class TestModel:
+        __tablename__ = "test"
+
+        key = sqlalchemy.Column(sqlalchemy.String, primary_key=True)
+        mandatory = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+        optional = sqlalchemy.Column(sqlalchemy.String)
+
+    _db = layabase.load("sqlite:///:memory:", [layabase.CRUDController(TestModel)])
+    yield _db
+    layabase.testing.reset(_db)
+
+
+def test_health_details(db, monkeypatch):
+    monkeypatch.setattr(layabase.database_sqlalchemy, "datetime", DateTimeModuleMock)
+    health_status = layabase.check(db)
+    expected_result = (
+        "pass",
+        {
+            "sqlite:select": {
+                "componentType": "datastore",
+                "observedValue": "",
+                "status": "pass",
+                "time": "2018-10-11T15:05:05.663979",
+            }
+        },
+    )
+    assert expected_result == health_status
+
+
+def test_health_details_no_db(db):
+    with pytest.raises(Exception) as exception_info:
+        layabase.check(None)
+    assert "A database connection URL must be provided." == str(exception_info.value)
