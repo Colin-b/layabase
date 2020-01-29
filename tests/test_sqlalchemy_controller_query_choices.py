9575407fa0f43b2ab6395fd534c4d543b6e1d82a
sc5890 <sc5890@engie.com>
2020-01-29 16:33:44 +0100

    Add more tests and correct incorrect once's

--- tests/test_sqlalchemy_controller_query_choices.py
+++ tests/test_sqlalchemy_controller_query_choices.py
@@ -14,8 +14,7 @@ def controller():
 
         key = sqlalchemy.Column(sqlalchemy.String, primary_key=True)
         test_field = sqlalchemy.Column(
-            sqlalchemy.String,
-            info={"marshmallow": {"choices": ["chose1", "chose2"]}},
+            sqlalchemy.String, info={"marshmallow": {"choices": ["chose1", "chose2"]}}
         )
 
     controller = layabase.CRUDController(TestTable)
@@ -28,14 +27,13 @@ def app(controller):
     application = flask.Flask(__name__)
     application.testing = True
     api = flask_restplus.Api(application)
-    namespace = api.namespace("Test", path="/")
+    namespace = api.namespace("Test", path="/", validate=True)
 
     controller.namespace(namespace)
 
     @namespace.route("/test")
     class TestResource(flask_restplus.Resource):
         @namespace.expect(controller.query_get_parser)
-        @namespace.marshal_with(controller.get_response_model)
         def get(self):
             return []
 
@@ -56,12 +54,6 @@ def app(controller):
         def get(self):
             return controller.query_get_parser.parse_args()
 
-        def post(self):
-            return controller.query_delete_parser.parse_args()
-
-        def put(self):
-            return controller.query_delete_parser.parse_args()
-
         def delete(self):
             return controller.query_delete_parser.parse_args()
 
@@ -71,15 +63,23 @@ def app(controller):
 def test_query_get_parser_without_value_for_field(client):
     response = client.get("/test_parsers")
     assert response.status_code == 200
-    assert response.json == {'key': None, 'test_field': None, 'limit': None, 'offset': None, 'order_by': None}
+    assert response.json == {
+        "key": None,
+        "test_field": None,
+        "limit": None,
+        "offset": None,
+        "order_by": None,
+    }
 
 
 def test_query_get_parser_with_invalid_value_for_field(client):
     response = client.get("/test_parsers?test_field=chose3")
     assert response.status_code == 400
     assert response.json == {
-        'errors': {'test_field': "The value 'chose3' is not a valid choice for 'test_field'."},
-        'message': 'Input payload validation failed'
+        "errors": {
+            "test_field": "The value 'chose3' is not a valid choice for 'test_field'."
+        },
+        "message": "Input payload validation failed",
     }
 
 
@@ -87,35 +87,51 @@ def test_query_get_parser_with_valid_and_invalid_value_for_field(client):
     response = client.get("/test_parsers?test_field=chose1&test_field=chose3")
     assert response.status_code == 400
     assert response.json == {
-        'errors': {'test_field': "The value 'chose3' is not a valid choice for 'test_field'."},
-        'message': 'Input payload validation failed'
+        "errors": {
+            "test_field": "The value 'chose3' is not a valid choice for 'test_field'."
+        },
+        "message": "Input payload validation failed",
     }
 
 
 def test_query_get_parser_with_valid_value_for_field(client):
     response = client.get("/test_parsers?test_field=chose1")
     assert response.status_code == 200
-    assert response.json == {'key': None, 'test_field': ['chose1'], 'limit': None, 'offset': None, 'order_by': None}
+    assert response.json == {
+        "key": None,
+        "test_field": ["chose1"],
+        "limit": None,
+        "offset": None,
+        "order_by": None,
+    }
 
 
 def test_query_get_parser_with_valid_values_for_field(client):
-    response = client.post("/test_parsers?test_field=chose1&test_field=chose2")
+    response = client.get("/test_parsers?test_field=chose1&test_field=chose2")
     assert response.status_code == 200
-    assert response.json == {'key': None, 'test_field': ['chose1', 'chose2']}
+    assert response.json == {
+        "key": None,
+        "limit": None,
+        "offset": None,
+        "order_by": None,
+        "test_field": ["chose1", "chose2"],
+    }
 
 
 def test_query_delete_parser_without_value_for_field(client):
     response = client.delete("/test_parsers")
     assert response.status_code == 200
-    assert response.json == {'key': None, 'test_field': None}
+    assert response.json == {"key": None, "test_field": None}
 
 
 def test_query_delete_parser_with_invalid_value_for_field(client):
     response = client.delete("/test_parsers?test_field=chose3")
     assert response.status_code == 400
     assert response.json == {
-        'errors': {'test_field': "The value 'chose3' is not a valid choice for 'test_field'."},
-        'message': 'Input payload validation failed'
+        "errors": {
+            "test_field": "The value 'chose3' is not a valid choice for 'test_field'."
+        },
+        "message": "Input payload validation failed",
     }
 
 
@@ -123,93 +139,205 @@ def test_query_delete_parser_with_valid_and_invalid_value_for_field(client):
     response = client.delete("/test_parsers?test_field=chose1&test_field=chose3")
     assert response.status_code == 400
     assert response.json == {
-        'errors': {'test_field': "The value 'chose3' is not a valid choice for 'test_field'."},
-        'message': 'Input payload validation failed'
+        "errors": {
+            "test_field": "The value 'chose3' is not a valid choice for 'test_field'."
+        },
+        "message": "Input payload validation failed",
     }
 
 
 def test_query_delete_parser_with_valid_value_for_field(client):
     response = client.delete("/test_parsers?test_field=chose1")
     assert response.status_code == 200
-    assert response.json == {'key': None, 'test_field': ['chose1'], }
+    assert response.json == {"key": None, "test_field": ["chose1"]}
 
 
 def test_query_delete_parser_with_valid_values_for_field(client):
     response = client.delete("/test_parsers?test_field=chose1&test_field=chose2")
     assert response.status_code == 200
-    assert response.json == {'key': None, 'test_field': ['chose1', 'chose2']}
+    assert response.json == {"key": None, "test_field": ["chose1", "chose2"]}
 
-def test_query_put_parser_without_value_for_field(client):
-    response = client.put("/test_parsers")
+
+def test_post_controller_with_valid_value_for_field(client):
+    response = client.post("/test", json={"key": "1", "test_field": "chose2"})
     assert response.status_code == 200
-    assert response.json == {'key': None, 'test_field': None}
 
 
-def test_query_put_parser_with_invalid_value_for_field(client):
-    response = client.put("/test_parsers?test_field=chose3")
+def test_post_controller_with_invalid_value_for_field(client):
+    response = client.post("/test", json={"key": "1", "test_field": "chose_invalid"})
     assert response.status_code == 400
     assert response.json == {
-        'errors': {'test_field': "The value 'chose3' is not a valid choice for 'test_field'."},
-        'message': 'Input payload validation failed'
+        "errors": {"test_field": "'chose_invalid' is not one of ['chose1', 'chose2']"},
+        "message": "Input payload validation failed",
     }
 
 
-def test_query_put_parser_with_valid_and_invalid_value_for_field(client):
-    response = client.put("/test_parsers?test_field=chose1&test_field=chose3")
+def test_post_controller_with_empty_value_for_field(client):
+    response = client.post("/test", json={"key": "1", "test_field": ""})
     assert response.status_code == 400
     assert response.json == {
-        'errors': {'test_field': "The value 'chose3' is not a valid choice for 'test_field'."},
-        'message': 'Input payload validation failed'
+        "errors": {"test_field": "'' is not one of ['chose1', 'chose2']"},
+        "message": "Input payload validation failed",
     }
-
-
-def test_query_put_parser_with_valid_value_for_field(client):
-    response = client.put("/test_parsers?test_field=chose1")
-    assert response.status_code == 200
-    assert response.json == {'key': None, 'test_field': ['chose1'], }
-
-
-def test_query_put_parser_with_valid_values_for_field(client):
-    response = client.put("/test_parsers?test_field=chose1&test_field=chose2")
-    assert response.status_code == 200
-    assert response.json == {'key': None, 'test_field': ['chose1', 'chose2']}
-
-def test_query_post_parser_without_value_for_field(client):
-    response = client.post("/test_parsers")
-    assert response.status_code == 200
-    assert response.json == {'key': None, 'test_field': None}
-
-
-def test_query_post_parser_with_invalid_value_for_field(client):
-    response = client.post("/test_parsers?test_field=chose3")
-    assert response.status_code == 400
-    assert response.json == {
-        'errors': {'test_field': "The value 'chose3' is not a valid choice for 'test_field'."},
-        'message': 'Input payload validation failed'
-    }
-
-
-def test_query_post_parser_with_valid_and_invalid_value_for_field(client):
-    response = client.post("/test_parsers?test_field=chose1&test_field=chose3")
-    assert response.status_code == 400
-    assert response.json == {
-        'errors': {'test_field': "The value 'chose3' is not a valid choice for 'test_field'."},
-        'message': 'Input payload validation failed'
-    }
-
-
-def test_query_post_parser_with_valid_value_for_field(client):
-    response = client.post("/test_parsers?test_field=chose1")
-    assert response.status_code == 200
-    assert response.json == {'key': None, 'test_field': ['chose1'], }
-
-
-def test_query_post_parser_with_valid_values_for_field(client):
-    response = client.post("/test_parsers?test_field=chose1&test_field=chose2")
-    assert response.status_code == 200
-    assert response.json == {'key': None, 'test_field': ['chose1', 'chose2']}
 
 
 def test_open_api_definition(client):
     response = client.get("/swagger.json")
-    assert response.json == {'swagger': '2.0', 'basePath': '/', 'paths': {'/test': {'post': {'responses': {'200': {'description': 'Success'}}, 'operationId': 'post_test_resource', 'parameters': [{'name': 'payload', 'required': True, 'in': 'body', 'schema': {'$ref': '#/definitions/TestTable_PostRequestModel'}}], 'tags': ['Test']}, 'put': {'responses': {'200': {'description': 'Success'}}, 'operationId': 'put_test_resource', 'parameters': [{'name': 'payload', 'required': True, 'in': 'body', 'schema': {'$ref': '#/definitions/TestTable_PutRequestModel'}}], 'tags': ['Test']}, 'delete': {'responses': {'200': {'description': 'Success'}}, 'operationId': 'delete_test_resource', 'parameters': [{'name': 'key', 'in': 'query', 'type': 'array', 'items': {'type': 'string'}, 'collectionFormat': 'multi'}, {'name': 'test_field', 'in': 'query', 'type': 'array', 'items': {'type': 'string'}, 'collectionFormat': 'multi', 'enum': ['chose1', 'chose2']}], 'tags': ['Test']}, 'get': {'responses': {'200': {'description': 'Success', 'schema': {'$ref': '#/definitions/TestTable_GetResponseModel'}}}, 'operationId': 'get_test_resource', 'parameters': [{'name': 'key', 'in': 'query', 'type': 'array', 'items': {'type': 'string'}, 'collectionFormat': 'multi'}, {'name': 'test_field', 'in': 'query', 'type': 'array', 'items': {'type': 'string'}, 'collectionFormat': 'multi', 'enum': ['chose1', 'chose2']}, {'name': 'limit', 'in': 'query', 'type': 'integer', 'minimum': 0, 'exclusiveMinimum': True}, {'name': 'offset', 'in': 'query', 'type': 'integer', 'minimum': 0}, {'name': 'order_by', 'in': 'query', 'type': 'array', 'items': {'type': 'string'}, 'collectionFormat': 'multi'}, {'name': 'X-Fields', 'in': 'header', 'type': 'string', 'format': 'mask', 'description': 'An optional fields mask'}], 'tags': ['Test']}}, '/test_parsers': {'post': {'responses': {'200': {'description': 'Success'}}, 'operationId': 'post_test_parsers_resource', 'tags': ['Test']}, 'put': {'responses': {'200': {'description': 'Success'}}, 'operationId': 'put_test_parsers_resource', 'tags': ['Test']}, 'delete': {'responses': {'200': {'description': 'Success'}}, 'operationId': 'delete_test_parsers_resource', 'tags': ['Test']}, 'get': {'responses': {'200': {'description': 'Success'}}, 'operationId': 'get_test_parsers_resource', 'tags': ['Test']}}}, 'info': {'title': 'API', 'version': '1.0'}, 'produces': ['application/json'], 'consumes': ['application/json'], 'tags': [{'name': 'Test'}], 'definitions': {'TestTable_PostRequestModel': {'required': ['key'], 'properties': {'key': {'type': 'string', 'readOnly': False, 'example': 'sample_value'}, 'test_field': {'type': 'string', 'readOnly': False, 'example': 'sample_value'}}, 'type': 'object'}, 'TestTable_PutRequestModel': {'required': ['key'], 'properties': {'key': {'type': 'string', 'readOnly': False, 'example': 'sample_value'}, 'test_field': {'type': 'string', 'readOnly': False, 'example': 'sample_value'}}, 'type': 'object'}, 'TestTable_GetResponseModel': {'required': ['key'], 'properties': {'key': {'type': 'string', 'readOnly': False, 'example': 'sample_value'}, 'test_field': {'type': 'string', 'readOnly': False, 'example': 'sample_value'}}, 'type': 'object'}}, 'responses': {'ParseError': {'description': "When a mask can't be parsed"}, 'MaskError': {'description': 'When any error occurs on mask'}}}
+    assert response.json == {
+        "swagger": "2.0",
+        "basePath": "/",
+        "paths": {
+            "/test": {
+                "get": {
+                    "responses": {"200": {"description": "Success"}},
+                    "operationId": "get_test_resource",
+                    "parameters": [
+                        {
+                            "name": "key",
+                            "in": "query",
+                            "type": "array",
+                            "items": {"type": "string"},
+                            "collectionFormat": "multi",
+                        },
+                        {
+                            "name": "test_field",
+                            "in": "query",
+                            "type": "array",
+                            "items": {"type": "string"},
+                            "collectionFormat": "multi",
+                            "enum": ["chose1", "chose2"],
+                        },
+                        {
+                            "name": "limit",
+                            "in": "query",
+                            "type": "integer",
+                            "minimum": 0,
+                            "exclusiveMinimum": True,
+                        },
+                        {
+                            "name": "offset",
+                            "in": "query",
+                            "type": "integer",
+                            "minimum": 0,
+                        },
+                        {
+                            "name": "order_by",
+                            "in": "query",
+                            "type": "array",
+                            "items": {"type": "string"},
+                            "collectionFormat": "multi",
+                        },
+                    ],
+                    "tags": ["Test"],
+                },
+                "post": {
+                    "responses": {"200": {"description": "Success"}},
+                    "operationId": "post_test_resource",
+                    "parameters": [
+                        {
+                            "name": "payload",
+                            "required": True,
+                            "in": "body",
+                            "schema": {
+                                "$ref": "#/definitions/TestTable_PostRequestModel"
+                            },
+                        }
+                    ],
+                    "tags": ["Test"],
+                },
+                "put": {
+                    "responses": {"200": {"description": "Success"}},
+                    "operationId": "put_test_resource",
+                    "parameters": [
+                        {
+                            "name": "payload",
+                            "required": True,
+                            "in": "body",
+                            "schema": {
+                                "$ref": "#/definitions/TestTable_PutRequestModel"
+                            },
+                        }
+                    ],
+                    "tags": ["Test"],
+                },
+                "delete": {
+                    "responses": {"200": {"description": "Success"}},
+                    "operationId": "delete_test_resource",
+                    "parameters": [
+                        {
+                            "name": "key",
+                            "in": "query",
+                            "type": "array",
+                            "items": {"type": "string"},
+                            "collectionFormat": "multi",
+                        },
+                        {
+                            "name": "test_field",
+                            "in": "query",
+                            "type": "array",
+                            "items": {"type": "string"},
+                            "collectionFormat": "multi",
+                            "enum": ["chose1", "chose2"],
+                        },
+                    ],
+                    "tags": ["Test"],
+                },
+            },
+            "/test_parsers": {
+                "delete": {
+                    "responses": {"200": {"description": "Success"}},
+                    "operationId": "delete_test_parsers_resource",
+                    "tags": ["Test"],
+                },
+                "get": {
+                    "responses": {"200": {"description": "Success"}},
+                    "operationId": "get_test_parsers_resource",
+                    "tags": ["Test"],
+                },
+            },
+        },
+        "info": {"title": "API", "version": "1.0"},
+        "produces": ["application/json"],
+        "consumes": ["application/json"],
+        "tags": [{"name": "Test"}],
+        "definitions": {
+            "TestTable_PostRequestModel": {
+                "required": ["key"],
+                "properties": {
+                    "key": {
+                        "type": "string",
+                        "readOnly": False,
+                        "example": "sample_value",
+                    },
+                    "test_field": {
+                        "type": "string",
+                        "readOnly": False,
+                        "example": "chose1",
+                        "enum": ["chose1", "chose2"],
+                    },
+                },
+                "type": "object",
+            },
+            "TestTable_PutRequestModel": {
+                "required": ["key"],
+                "properties": {
+                    "key": {
+                        "type": "string",
+                        "readOnly": False,
+                        "example": "sample_value",
+                    },
+                    "test_field": {
+                        "type": "string",
+                        "readOnly": False,
+                        "example": "chose1",
+                        "enum": ["chose1", "chose2"],
+                    },
+                },
+                "type": "object",
+            },
+        },
+        "responses": {
+            "ParseError": {"description": "When a mask can't be parsed"},
+            "MaskError": {"description": "When any error occurs on mask"},
+        },
+    }
