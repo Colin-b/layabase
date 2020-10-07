165ef9a77ced4d4136beca3011ead2e890f79f7b
Colin-b <Colin-b@users.noreply.github.com>
2020-10-07 19:46:40 +0200

    Remove support for python 3.9 as there is an issue with coverage for now

--- setup.py
+++ setup.py
@@ -60,7 +60,6 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Topic :: Software Development :: Build Tools",
     ],
     keywords=["mongo", "sqla", "db", "flask"],
